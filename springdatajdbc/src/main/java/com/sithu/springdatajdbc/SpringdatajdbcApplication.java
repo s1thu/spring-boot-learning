c84bc8ade1ecad7f0130dc1960c3d20f996722a5
s1thu <s1thu.w1n.5601@gmail.com>
2025-01-02 21:34:55 +0630

    Spring JDBC Template,h2 database complete(CURD)

--- springdatajdbc/src/main/java/com/sithu/springdatajdbc/SpringdatajdbcApplication.java
+++ springdatajdbc/src/main/java/com/sithu/springdatajdbc/SpringdatajdbcApplication.java
@@ -1,5 +1,6 @@
 package com.sithu.springdatajdbc;
 
+import com.sithu.springdatajdbc.lesson.bean.Player;
 import com.sithu.springdatajdbc.lesson.dao.PlayerDao;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringdatajdbcApplication implements CommandLineRunner {
@@ -22,6 +26,20 @@ public class SpringdatajdbcApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        logger.info("Get Player from where id = 1 : {}", dao.getPlayerId(1));
+        logger.info("Insert Into Player 4: {}",dao.insertPlayer(
+                new Player(4,"Sithu","Myanmar",new Date(System.currentTimeMillis()),17)
+        ));
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = dateFormat.parse("2001-06-05");
+
+        logger.info("Update Player Sithu to Sithu Win : {}",dao.updatePlayer(
+                new Player(4,"SithuWin","Myanmar", date,17)
+        ));
         logger.info("All players data: {}",dao.getAllPlayers());
+        logger.info("Delete Player id : 4 {}", dao.deletePlayer(1));
     }
+
+
 }
