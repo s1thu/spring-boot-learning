9e8f4df462057cbc04f443fd1cc9ff197e9b22df
s1thu <s1thu.w1n.5601@gmail.com>
2025-01-02 22:12:01 +0630

    Spring Data JPA Start

---
+++ springDataJpa/src/main/java/com/sithu/springdatajpa/entities/Player.java
@@ -0,0 +1,88 @@
+package com.sithu.springdatajpa.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Player_jpa")
+@NamedQuery(name="get_all_players", query = "select p from Player p")
+public class Player {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "nationality")
+    private String nationality;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @Column(name = "titles")
+    private int titles;
+
+    public Player(String name, String nationality, Date birthDate, int titles) {
+        this.name = name;
+        this.nationality = nationality;
+        this.birthDate = birthDate;
+        this.titles = titles;
+    }
+
+    public Player() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getTitles() {
+        return titles;
+    }
+
+    public void setTitles(int titles) {
+        this.titles = titles;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", birthDate=" + birthDate +
+                ", titles=" + titles +
+                '}';
+    }
+}
