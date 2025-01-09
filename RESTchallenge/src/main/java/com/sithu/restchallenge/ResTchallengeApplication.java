7263ea8e42e1236022a9a93feadc42c61a3b9943
s1thu <s1thu.w1n.5601@gmail.com>
2025-01-09 22:07:40 +0630

    add to RestChallenge

---
+++ RESTchallenge/src/main/java/com/sithu/restchallenge/ResTchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.sithu.restchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTchallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTchallengeApplication.class, args);
+    }
+
+}
---
+++ RESTchallenge/src/test/java/com/sithu/restchallenge/ResTchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sithu.restchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTchallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
